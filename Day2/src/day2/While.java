1eba4066f50283f5776f11067463d505dd222054
Aishwarya-Kannalli <kaishwarya4142gmail.com>
2023-08-14 12:17:41 +0530

    Example

---
+++ Day2/src/day2/While.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class While {
+	   public static void main(String[] args) {
+	        int count = 1;
+
+	        while (count <= 5) {
+	            System.out.println("Count: " + count);
+	            count++;
+	        }
+	    }
+}
+
