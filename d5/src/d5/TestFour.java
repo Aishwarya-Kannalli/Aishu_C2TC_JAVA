8a7abfa33d861e14eee62d261bb90ab85f0cf4f2
Aishwarya-Kannalli <kaishwarya4142gmail.com>
2023-09-05 13:10:35 +0530

    Codes

---
+++ d5/src/d5/TestFour.java
@@ -0,0 +1,16 @@
+package d5;
+
+import d5.TestOne;
+
+public class TestFour extends TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestFour t4=new TestFour();
+		t4.id=5;
+		System.out.println(t4.id);
+		t4.display();
+
+	}
+
+}
